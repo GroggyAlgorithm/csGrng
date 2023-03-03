89d6d18354fb1ffa9a3b9577c09a0a35516567bd
Tim Robbins <GroggyAlgorithm@gmail.com>
2023-03-03 15:50:04 -0500

    Add files via upload

--- grng/VoronoiNoise.cs
+++ grng/VoronoiNoise.cs
@@ -1,3 +1,54 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class VoronoiNoise : INoise
+// {
+//     public float displacement = 0f;
+
+// 	public VoronoiNoise() : base()
+// 	{
+// 	}
+
+// 	public VoronoiNoise(int seed, float displacement = 1.0f) : base(seed)
+//     {
+//         this.seed = seed;
+//         this.displacement = displacement;
+//     }
+
+//     public VoronoiNoise(GRNG grng, float displacement = 1.0f) : base(grng)
+//     {
+//         this.displacement = displacement;
+//     }
+    
+//     public VoronoiNoise(INoise noiser, float displacement = 1.0f) : base(noiser)
+//     {
+//         this.displacement = displacement;
+//     }
+
+//     public override float Sample1D(float xPosition, bool useDistance = false)
+//     {
+//         if (amplitude == 0) amplitude = 1;
+//         return gRand.Voronoi(xPosition, frequency, useDistance, displacement) * amplitude;
+//     }
+
+//     public override float Sample2D(float xPosition, float yPosition, bool useDistance = false)
+//     {
+//         if (amplitude == 0) amplitude = 1;
+//         return gRand.Voronoi(xPosition, yPosition, frequency, useDistance, displacement) * amplitude;
+//     }
+
+//     public override float Sample3D(float xPosition, float yPosition, float zPosition, bool useDistance = false)
+//     {
+//         if (amplitude == 0) amplitude = 1;
+//         return gRand.Voronoi(xPosition, yPosition, zPosition, frequency, useDistance, displacement) * amplitude;
+//     }
+
+    
+// }
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 
